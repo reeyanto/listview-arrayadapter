692c35a07c86d0b0592beff7bf6232a29bb2e6ef
Riyanto <riyanto.droider@gmail.com>
2020-08-24 06:43:17 +0700

    Upload the code

---
+++ app/src/main/java/id/pinterkom/listviewexample/MainActivity.java
@@ -0,0 +1,32 @@
+package id.pinterkom.listviewexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] weekdays = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+    ListView lv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lv = findViewById(R.id.lv);
+        lv.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, weekdays));
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), weekdays[i] + " clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
